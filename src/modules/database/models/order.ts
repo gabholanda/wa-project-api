9819e68164fffa5779dc2a59017da60b1f011a0d
Gabriel Holanda <gabriel_ebot@hotmail.com>
2020-07-19 23:32:05 -0300

    WIP

--- src/modules/database/models/order.ts
+++ src/modules/database/models/order.ts
@@ -2,12 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Model } from 'objection';
 
 export class Order extends Model {
-    @ApiProperty({ type: 'integer' })
-    public id?: number
-    @ApiProperty({ type: 'string' })
-    public description: string
-    @ApiProperty({ type: 'number' })
-    public quantity: number
-    @ApiProperty({ type: 'number' })
-    public value: number
+  @ApiProperty({ type: 'integer' })
+  public id?: number;
+  @ApiProperty({ type: 'string' })
+  public description: string;
+  @ApiProperty({ type: 'number' })
+  public quantity: number;
+  @ApiProperty({ type: 'number' })
+  public value: number;
 }
